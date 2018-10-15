b0a7d02ea9b4e0758606d794420a5c8dd72a0adc
joshikeshavr <43806651+joshikeshavr@users.noreply.github.com>
2018-10-15 14:35:22 +0530

    Update addressbook.java

--- src/test/java/TestNG/addressbook.java
+++ src/test/java/TestNG/addressbook.java
@@ -14,7 +14,7 @@ public class addressbook {
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		  driver = new ChromeDriver();
-		  driver.get("http://35.231.231.15:32768/addressbook/");
+		  driver.get("http://35.154.250.255:8080/addressbook/");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 	}
